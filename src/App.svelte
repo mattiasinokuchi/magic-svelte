5071f7d2cb0a4e99f78fb0cc78640ed28ce04049
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-06-14 16:49:17 +0200

    added todos as paragraphs

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import Nav from "./Nav.svelte";
+  import List from "./List.svelte";
 </script>
 
 <Nav></Nav>
+<List></List>
---
+++ src/List.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { todos } from "./stores";
+
+    let remove;
+    let update;
+</script>
+
+<form>
+    {#if $todos}
+        {#each $todos.data as { data }}
+            <p>{data.title}</p>
+        {/each}
+    {/if}
+</form>
