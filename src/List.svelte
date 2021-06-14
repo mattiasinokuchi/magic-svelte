163226a3287cc012baa462eddaeba51e7b7749b0
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-06-14 17:01:18 +0200

    changed todos into inputs

--- src/List.svelte
+++ src/List.svelte
@@ -1,14 +1,55 @@
 <script>
     import { todos } from "./stores";
 
-    let remove;
-    let update;
+    const update = (num) =>
+        function () {
+            try {
+                fetch("/.netlify/functions/update", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: $todos[num - 1]["ref"]["@ref"]["id"],
+                        data: $todos[num - 1]["data"]["name"],
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+    const remove = (num) =>
+        function () {
+            try {
+                fetch("/.netlify/functions/delete", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: $todos[num - 1]["ref"]["@ref"]["id"],
+                        data: $todos[num - 1]["data"]["name"],
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                $todos = [...$todos.slice(0, num - 1), ...$todos.slice(num)];
+            } catch (error) {
+                console.log(error);
+            }
+        };
 </script>
 
 <form>
     {#if $todos}
-        {#each $todos.data as { data }}
-            <p>{data.title}</p>
+        {#each $todos.data as { data }, i}
+            <div id="todo">
+                <button on:click|preventDefault={remove(i + 1)}>🗑</button>
+                <input
+                    bind:value={data.title}
+                    on:change={update(i + 1)}
+                    size={data.title.length}
+                    maxlength="35"
+                />
+            </div>
         {/each}
     {/if}
 </form>
