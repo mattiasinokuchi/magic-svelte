d0ee11eae6be9d186dde45da571d6aa4b403ba51
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-06-16 13:25:27 +0200

    added component

---
+++ src/Form.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { todos } from "./stores";
+    import { didToken } from "./stores";
+
+    let newTodo;
+    async function submit() {
+        try {
+            const object = { data: { title: newTodo } };
+            newTodo = "";
+            $todos = $todos.concat(object);
+            const response = await fetch("/.netlify/functions/create", {
+                method: "POST",
+                body: JSON.stringify({ newTodo: object.data.title }),
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${$didToken}`,
+                },
+            });
+            $todos = await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+    <input
+        type="text"
+        bind:value={newTodo}
+        name="newTodo"
+        placeholder="Type here and..."
+        maxlength="35"
+        size="15"
+    />
+    <input type="submit" value="Add it" />
+</form>
+
+<style>
+    form {
+        position: fixed;
+        bottom: 20vh;
+        margin-left: 15%;
+        margin-right: 15%;
+        width: 70%;
+    }
+
+    input {
+        font-size: 4vh;
+        border-style: none;
+    }
+</style>
