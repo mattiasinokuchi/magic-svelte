857f761ee2ea278e79b27828e6e5aaaafe2b4cf1
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-06-25 06:17:58 +0200

    input not transparent

--- src/Form.svelte
+++ src/Form.svelte
@@ -49,16 +49,10 @@
     input {
         margin: 3vh;
         font-size: 3vh;
+        color: orange;
+        background-color: white;
         font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
         margin: 1vh;
-        /* Set the background color */
-        background: linear-gradient(to bottom, orangered 50%, white 100%);
-        /* Mask the color to the text, and remove the rest  */
-        -webkit-background-clip: text;
-        background-clip: text;
-        /* Make the text fill color value transparent so the masked background color comes through */
-        -webkit-text-fill-color: transparent;
         border: 1px solid orange;
-        border-image: linear-gradient(to bottom, orange 50%, white 100%) 1;
     }
 </style>
