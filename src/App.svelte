344a660b36f47d27b3af1a94ac3a3406d51303f1
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-06-27 11:04:28 +0200

    changed names on components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
   import Heading from "./Heading.svelte";
-  import List from "./List.svelte";
-  import AddButton from "./AddButton.svelte";
+  import TodoList from "./TodoList.svelte";
+  import TodoInput from "./TodoInput.svelte";
 </script>
 
 <Heading />
-<List />
-<AddButton />
+<TodoList />
+<TodoInput />
